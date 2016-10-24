47356ed3de9b37203ad3429f3aa5a409a7abaf5d
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-10-24 12:43:51 -0500

    Commit

--- Zip_Codes/Form1.Designer.vb
+++ Zip_Codes/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,79 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lstCities = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblZipCodes = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&City:"
+        '
+        'lstCities
+        '
+        Me.lstCities.FormattingEnabled = True
+        Me.lstCities.Location = New System.Drawing.Point(13, 30)
+        Me.lstCities.Name = "lstCities"
+        Me.lstCities.Size = New System.Drawing.Size(120, 95)
+        Me.lstCities.Sorted = True
+        Me.lstCities.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(168, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "ZIP code(s):"
+        '
+        'lblZipCodes
+        '
+        Me.lblZipCodes.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblZipCodes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblZipCodes.Location = New System.Drawing.Point(151, 30)
+        Me.lblZipCodes.Name = "lblZipCodes"
+        Me.lblZipCodes.Size = New System.Drawing.Size(120, 95)
+        Me.lblZipCodes.TabIndex = 3
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(12, 131)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(260, 33)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(284, 178)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblZipCodes)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lstCities)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Zip Codes"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lstCities As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblZipCodes As Label
+    Friend WithEvents btnExit As Button
 End Class
--- Zip_Codes/Form1.vb
+++ Zip_Codes/Form1.vb
@@ -1,3 +1,47 @@
-﻿Public Class Form1
+﻿'Joshua Pickenpaugh
+'October 24th, 2016
+'Zip Codes (non-OOP)
 
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'Closes the app:
+        Me.Close()
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Adds names to the list box on load:
+        lstCities.Items.Add("Park Ridge")
+        lstCities.Items.Add("Barrington")
+        lstCities.Items.Add("Glen Ellyn")
+        lstCities.Items.Add("Algonquin")
+        lstCities.Items.Add("Crystal Lake")
+
+    End Sub
+
+    Private Sub lstCities_SelectedValueChanged(sender As Object, e As EventArgs) Handles lstCities.SelectedValueChanged
+
+        Dim strCity As String
+
+        strCity = lstCities.Text
+
+        Select Case strCity
+            Case "Park Ridge"
+                lblZipCodes.Text = "60068"
+            Case "Barrington"
+                lblZipCodes.Text = "60010, 60011"
+            Case "Glen Ellyn"
+                lblZipCodes.Text = "60137, 60138"
+            Case "Algonquin"
+                lblZipCodes.Text = "60102"
+            Case "Crystal Lake"
+                lblZipCodes.Text = "60012"
+            Case Else
+                lblZipCodes.Text = "(n/a)"
+
+        End Select
+
+    End Sub
 End Class
